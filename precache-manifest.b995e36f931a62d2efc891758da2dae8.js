8bd313983b56984920ff3df9dd9b054e114815bd
youngwoo nam <tynahan@gmail.com>
2020-01-10 17:04:10 +0900

    Updates

---
+++ precache-manifest.b995e36f931a62d2efc891758da2dae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1c2b57501eb78e75550cdc50e34e04",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "38d4477667bea1488dd4",
+    "url": "/movie-app/static/css/main.095a78e3.chunk.css"
+  },
+  {
+    "revision": "c529b2fc5b41c1dd1c21",
+    "url": "/movie-app/static/js/2.e8ee7b20.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-app/static/js/2.e8ee7b20.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38d4477667bea1488dd4",
+    "url": "/movie-app/static/js/main.f98d83b0.chunk.js"
+  },
+  {
+    "revision": "7a79ff4d5cfe5c3dbc42",
+    "url": "/movie-app/static/js/runtime-main.8a35f832.js"
+  }
+]);
