d31daebeb71326866a30ae7acf910832ac2ec80d
youngwoo nam <tynahan@gmail.com>
2020-01-11 10:03:10 +0900

    Getting Ready for the Router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,7 @@
 import React from "react";
-import Axios from "axios";
-import "./App.css";
-import Movie from "./Movie";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await Axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ isLoading: false, movies });
-  };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return <span>something</span>;
 }
 
 export default App;
